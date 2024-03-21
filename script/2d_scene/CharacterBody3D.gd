c16206c5f63aa95614c7c894365c3851bf4f1c7e
Moeva <mondohamoeva@hotmail.fr>
2024-03-21 08:08:19 +0100

    {ajout Hub}

--- script/2d_scene/CharacterBody3D.gd
+++ script/2d_scene/CharacterBody3D.gd
@@ -2,14 +2,17 @@ extends CharacterBody3D
 
 
 @export var speed = 10000	
-
+@export var player_sprite: AnimatedSprite3D
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	print(direction)
+	if velocity != Vector3.ZERO:
+		player_sprite.play("walk")
+	else:
+		player_sprite.play("idle")
 	if direction:
 		velocity.x = direction.x * speed * delta
 		velocity.z = direction.z * speed * delta
