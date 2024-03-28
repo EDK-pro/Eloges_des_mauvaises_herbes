131c4240dfef11291b10628acded7e9452c77209
Moeva <mondohamoeva@hotmail.fr>
2024-03-28 21:15:26 +0100

    {final touch before demo}

--- assets/prefabs/guitare_samuel.gd
+++ assets/prefabs/guitare_samuel.gd
@@ -13,3 +13,9 @@ func _on_body_entered(body):
 	if str(body).get_slice(":",0) == "Player":
 		var tween = get_tree().create_tween()
 		tween.tween_property(message, "modulate:a", 1, 0.5).set_trans(Tween.TRANS_CUBIC)
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if str(body).get_slice(":",0) == "Player":
+		var tween = get_tree().create_tween()
+		tween.tween_property(message, "modulate:a", 0, 0.5).set_trans(Tween.TRANS_CUBIC)
