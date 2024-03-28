0c808440aaac13b6d47dcafddb5555931c659be1
Moeva <mondohamoeva@hotmail.fr>
2024-03-28 21:22:06 +0100

    {fixing guitare}

--- assets/prefabs/guitare_samuel.gd
+++ assets/prefabs/guitare_samuel.gd
@@ -2,20 +2,23 @@ extends Area3D
 
 @export var message: RichTextLabel
 @export var samuel: AudioStreamPlayer3D
+@export var canPlay = false
 
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("play_guitar"):
-		samuel.play()
+	if Input.is_action_just_pressed("play_guitar") and canPlay:
+		samuel.play() 
 		return
 
 func _on_body_entered(body):
 	if str(body).get_slice(":",0) == "Player":
+		canPlay = true
 		var tween = get_tree().create_tween()
 		tween.tween_property(message, "modulate:a", 1, 0.5).set_trans(Tween.TRANS_CUBIC)
 
 
 func _on_body_exited(body: Node3D) -> void:
 	if str(body).get_slice(":",0) == "Player":
+		canPlay = false
 		var tween = get_tree().create_tween()
 		tween.tween_property(message, "modulate:a", 0, 0.5).set_trans(Tween.TRANS_CUBIC)
